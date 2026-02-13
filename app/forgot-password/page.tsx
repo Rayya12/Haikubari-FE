70dff8c48abf68dad815cce9fafe224ae4b1e35a
Rayya12 <rayaalului@gmail.com>
2026-02-13 13:15:37 +0700

    feat: menambahkan page forgotPassword

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Image from "next/image";
+import Form from "next/form";
+import Link from "next/link";
+import { useActionState } from "react";
+import { handleLogin } from "../lib/action";
+import { useState } from "react";
+
+export default function ForgotPasswordPage() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <div
+      className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center space-y-4"
+      style={{ backgroundImage: "url(/loginBackground.png)" }}
+    >
+      <Image
+        src="/blacklogo.png"
+        alt="Haikubari Logo"
+        width={500}
+        height={500}
+      />
+
+      <div className="flex justify-center lg:w-1/3 sm:w-full space-x-4 p-4 border border-gray-300 rounded-lg shadow-lg">
+      <div className="flex flex-col items-center w-full">
+        <h1 className="flex mb-4 text-2xl font-bold">お持ちするメールアドレス入れて下さい</h1>
+
+        <div className="flex text-black text-left font-bold w-3/4">
+          <label htmlFor="email">メール</label>
+        </div>
+
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="w-3/4 border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-ateneo-blue text-black"
+          value={email}
+          onChange={handleEmail}
+        />
+
+        {error && (
+          <div className="w-3/4 bg-red-300 p-2 rounded-md border border-red-700 text-red-950 mt-2">
+            {error}
+          </div>
+        )}
+
+        <button
+          className="bg-lime-green text-white p-4 rounded-md shadow-md w-3/4 mt-4 mb-4 hover:ring-2 hover:ring-teal"
+          type="submit"
+        >
+          確認
+        </button>
+
+        <Link
+          className="text-ateneo-blue hover:font-bold"
+          href={"/login"}
+        >
+            ログイン場面へ戻ります
+        </Link>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
