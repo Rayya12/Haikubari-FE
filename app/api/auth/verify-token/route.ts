5407b8ddab7ca327eafd2be3a91fd6fc33c2392a
Rayya12 <rayaalului@gmail.com>
2026-02-14 10:26:20 +0700

    fix: complete for reset password

--- app/api/auth/verify-token/route.ts
+++ app/api/auth/verify-token/route.ts
@@ -1,20 +1,19 @@
-import { redirect } from "next/navigation"
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 export async function POST(request: Request) {
     const {token,password} = await request.json()
 
-    const response = await fetch(`${BACKEND_URL}/auth/forgot-password`,{
+    const response = await fetch(`${BACKEND_URL}/auth/reset-password`,{
         method : "POST",
         headers : {
             'Content-Type' : 'application/json'
         },
-        body : JSON.stringify({token,password})
+        body : JSON.stringify({"token":String(token),"password":String(password)})
     })
 
     if (!response.ok){
-        return new Response(JSON.stringify({error : "トークンが間違いました"}))
+        return new Response(JSON.stringify({error : "トークンが間違いました"}),{status:400})
     }
 
     return Response.json({success : true})
--- app/verify-token/page.tsx
+++ app/verify-token/page.tsx
@@ -90,18 +90,18 @@ export default function VerifOTP() {
                 <div className="flex justify-start w-3/4">
                     <label htmlFor="token" className="font-bold">トークン</label>
                 </div>
-                <input type="text" required id="token" name="token" className="flex p-2 border border-black text-black w-3/4 rounded-md" value={token} onChange={handleToken}/>
+                <input type="text" required id="token" name="token" className="flex p-2 border border-black text-black w-3/4 rounded-md focus:outline-1 focus:outline-lime-green" value={token} onChange={handleToken}/>
 
 
                 <div className="flex justify-start w-3/4 mt-2">
                     <label htmlFor="password" className="font-bold">パスワード</label>
                 </div>
-                <input type="text" required id="password" name="password" className="flex p-2 border border-black text-black w-3/4 rounded-md" value={password} onChange={handlePassword}/>
+                <input type="password" required id="password" name="password" className="flex p-2 border border-black text-black w-3/4 rounded-md focus:outline-1 hover:outline-lime-green" value={password} onChange={handlePassword}/>
 
                 <div className="flex justify-start w-3/4 mt-2">
                     <label htmlFor="passwordC" className="font-bold">パスワード確認</label>
                 </div>
-                <input type="text" required id="passwordC" name="passwordC" className="flex p-2 border border-black text-black w-3/4 rounded-md" value={passwordConfirm} onChange={handleConfirmPassword}/>
+                <input type="password" required id="passwordC" name="passwordC" className="flex p-2 border border-black text-black w-3/4 rounded-md focus:outline-1 hover:outline-lime-green" value={passwordConfirm} onChange={handleConfirmPassword}/>
                 </div>
                 {error && (
                 <div className="flex w-3/4 items-center justify-center bg-red-300 border-red-700 text-red-950 rounded-md p-2 mt-2">{error}</div>
