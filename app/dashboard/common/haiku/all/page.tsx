db54a474a1a46623f764e08d02ef891d1dd86224
Rayya12 <rayaalului@gmail.com>
2026-02-22 15:15:18 +0700

    feat: adding suspense for deployment

--- app/dashboard/common/haiku/all/page.tsx
+++ app/dashboard/common/haiku/all/page.tsx
@@ -2,11 +2,14 @@
 
 import AllHaikuList from "@/app/ui/AllHaikuList"
 import Router from "next/navigation";
+import { Suspense } from "react";
 
 export default function allHaiku(){
     return( 
-    <div className="bg-white min-h-screen">
-        <AllHaikuList />
-    </div>
+    <Suspense fallback = {<div>Loading...</div>}>
+        <div className="bg-white min-h-screen">
+            <AllHaikuList />
+        </div>
+    </Suspense>
     )
 }
--- app/dashboard/common/haiku/mine/page.tsx
+++ app/dashboard/common/haiku/mine/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import HaikuList from "@/app/ui/HaikuList"
 import Router from "next/navigation";
+import { Suspense } from "react";
 
 export default function myHaiku(){
     return(
-    <div className="bg-white min-h-screen">
-        <HaikuList />
-    </div>
+    <Suspense fallback = {<div>Loading...</div>}>
+        <div className="bg-white min-h-screen">
+            <HaikuList />
+        </div>
+    </Suspense>
     )
 }
