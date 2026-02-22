52013a647f6fcc68945602a0336e39c4189aed52
Rayya12 <rayaalului@gmail.com>
2026-02-22 16:42:27 +0700

    feat: tambahkan moji

--- app/lib/action.tsx
+++ app/lib/action.tsx
@@ -27,7 +27,7 @@ export async function  handleRegister(prevState : {error?:string} | null,formDat
     }
 
     if (!usernameRegex.test(username)){
-        return {error:"ユーサネームは文字と数字のみ(長さは12文字です)"}
+        return {error:"ユーサネームは文字と数字のみ(長さは8文字ー12文字です)"}
     }
 
     if (!email){
